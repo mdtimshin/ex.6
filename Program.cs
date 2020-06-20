8e3a5d7a679eb1b407b7278c09283a21466a1c5c
mdtimshin <63156611+mdtimshin@users.noreply.github.com>
2020-06-20 22:26:03 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -32,48 +32,57 @@ namespace ex._6
             return 0;
         }
 
-        static void Show (List<double> posl)
+        static void Show (List<double> posl, int N)
         {
-            foreach(double n in posl)
+            for(int i =0; i<N; i++)
             {
-                Console.Write(n + " ");
+                Console.Write(posl[i] + " ");
             }
             Console.WriteLine();
         }
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Введите первый член");
-            double a1 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите второй член");
-            double a2 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите третий член");
-            double a3 = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите M");
-            double M = double.Parse(Console.ReadLine());
-            Console.WriteLine("Введите кол-во элементов");
-            int N = int.Parse(Console.ReadLine());
-            posl.Add(a1);
-            posl.Add(a2);
-            posl.Add(a3);
-            AddElements(N);
-            Show(posl);
-            int count = 0;
-            foreach (double n in posl)
+            try
             {
-                if (n == M)
+                Console.WriteLine("Введите первый член");
+                double a1 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите второй член");
+                double a2 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите третий член");
+                double a3 = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите M");
+                double M = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите кол-во элементов");
+                int N = int.Parse(Console.ReadLine());
+                posl.Add(a1);
+                posl.Add(a2);
+                posl.Add(a3);
+                AddElements(N);
+                Show(posl, N);
+                int count = 0;
+                for (int i = 0; i < N; i++)
                 {
-                    count++;
+                    if (posl[i] == M)
+                    {
+                        count++;
+                    }
+                }
+                Console.WriteLine($"В последовательности найдено {count} элементов равных {M}");
+                if (count != 0)
+                {
+                    Console.WriteLine($"Номера элементов, равных {M}:");
+                    for (int i = 0; i < N; i++)
+                    {
+                        if (posl[i] == M)
+                        {
+                            Console.Write($"{i + 1} ");
+                        }
+                    }
                 }
             }
-            Console.WriteLine($"В последовательности найдено {count} элементов равных {M}");
-            Console.WriteLine($"Номера элементов, равных {M}:");
-            for (int i = 0; i < posl.Count; i++)
+            catch (System.FormatException)
             {
-                if (posl[i] == M)
-                {
-                    Console.Write($"{i + 1} ");
-                }
+                Console.WriteLine("Введите целое число");
             }
             Console.ReadKey();
         }
